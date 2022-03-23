b150126225389064c94b4723f4fab9355e4344e6
XiangPan <xiangpan@nyu.edu>
2022-03-22 23:34:08 -0400

    update

--- HW2/c.py
+++ HW2/c.py
@@ -1,7 +1,7 @@
 '''
 Author: Xiang Pan
 Date: 2022-03-08 00:58:37
-LastEditTime: 2022-03-08 03:58:08
+LastEditTime: 2022-03-22 13:43:41
 LastEditors: Xiang Pan
 Description: 
 FilePath: /HW2/c.py
@@ -9,4 +9,45 @@ FilePath: /HW2/c.py
 '''
 # adalone dataset
 import numpy as np
+
+# plot p_acc_list vs C for each d with subplot
+import matplotlib.pyplot as plt
+res = acc_res
+
+for i in range(len(res)):
+    # plt.figure(figsize=(20,20))
+    # plt.subplot(5,10,i+1)
+    plt.plot(100 - res[i])
+plt.legend(['d=1', 'd=2', 'd=3', 'd=4', 'd=5'])
+
+# plt.plot(p_acc_list)
+plt.title('Cross Val Error vs C')
+plt.xlabel('C')
+plt.ylabel('val_error')
+plt.xticks([i for i in range(20)],[f"3e{i}" for i in range(-10, 10)], rotation=40)
+# mark max
+plt.plot(c_star_index, 100 - res.max(), 'ro')
+# max lengend
+plt.annotate(f'{c_star}', xy=(c_star_index, res.max()), xytext=(c_star_index+0.5, 100 - res.max()+0.5), arrowprops=dict(facecolor='black', shrink=0.01))
+plt.savefig("./images/C3_cross_val_error_vs_d")
+
+
+
+for i in range(len(res)):
+    # plt.figure(figsize=(20,20))
+    # plt.subplot(5,10,i+1)
+    plt.plot((100 - res[i])/100))
+plt.legend(['d=1', 'd=2', 'd=3', 'd=4', 'd=5'])
+
+# plt.plot(p_acc_list)
+plt.title('(Cross Val Error + std) vs C')
+plt.xlabel('C')
+plt.ylabel('val_error')
+plt.xticks([i for i in range(20)],[f"3e{i}" for i in range(-10, 10)], rotation=40)
+# mark max
+plt.plot(c_star_index, 100 - res.max(), 'ro')
+# max lengend
+plt.annotate(f'{c_star}', xy=(c_star_index, res.max()), xytext=(c_star_index+0.5, 100 - res.max()+0.5), arrowprops=dict(facecolor='black', shrink=0.01))
+plt.savefig("./images/C3_cross_val_error_vs_d")
+
 
---
+++ HW2/prepare.sh
@@ -0,0 +1,11 @@
+###
+ # @Author: Xiang Pan
+ # @Date: 2022-03-20 02:57:03
+ # @LastEditTime: 2022-03-20 03:03:55
+ # @LastEditors: Xiang Pan
+ # @Description: 
+ # @FilePath: /HW2/prepare.sh
+ # @email: xiangpan@nyu.edu
+### 
+wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/abalone ./cached_datasets/abalone/
+wget https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/abalone_scale ./cached_datasets/abalone/
